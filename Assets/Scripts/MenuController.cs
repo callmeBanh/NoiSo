1a8ddf8fcfcb75d91b0a871a9b71f6af94f1bb59
huynhngocduy <huynhngocduy3122021@gmail.com>
2026-02-09 18:13:33 +0700

    feat: Update scene loading and add QuitGame functionality in MenuController

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,7 +6,7 @@ public class MenuController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
     {
-        LoadingController.LoadScene("testScene");
+        LoadingController.LoadScene("GamePlay");
     }
     public void BackToMainMenu()
     {
